a1c64c6a5e1cc973081b36f31403c645008e2282
Juchase <jcsetiawan45@gmail.com>
2025-07-12 01:41:51 +0800

    update react-router jadi react-router-dom

--- belajar-reactjs-contact-management/src/main.jsx
+++ belajar-reactjs-contact-management/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import UserRegister from "./components/Users/UserRegister";
 import UserLogin from "./components/Users/UserLogin";
