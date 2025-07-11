212b8627015bb3946cfc0752bc495c35dee73ec9
Juchase <jcsetiawan45@gmail.com>
2025-07-12 04:42:57 +0800

    add error handling on main.js

--- belajar-nodejs-restful-api-main/src/main.js
+++ belajar-nodejs-restful-api-main/src/main.js
@@ -1,6 +1,32 @@
-import {web} from "./application/web.js";
-import {logger} from "./application/logging.js";
+import { web } from "./application/web.js";
+import { logger } from "./application/logging.js";
+import { prismaClient } from "./application/database.js";
 
-web.listen(3000, () => {
-    logger.info("App start");
+async function startServer() {
+  try {
+    // Test Prisma connection
+    await prismaClient.$connect();
+    logger.info("Database connected");
+
+    // Start the web server
+    web.listen(3000, () => {
+      logger.info("App start");
+    });
+  } catch (error) {
+    logger.error("Failed to start server:", error);
+    process.exit(1);
+  }
+}
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (error) => {
+  logger.error("Uncaught Exception thrown:", error);
+  process.exit(1);
+});
+
+startServer();
