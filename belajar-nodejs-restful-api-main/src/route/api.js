1fb60f36d25b7fb95161cb74dae6557ac4cceb13
Juchase <jcsetiawan45@gmail.com>
2025-07-17 17:04:39 +0800

    fix all api path

--- belajar-nodejs-restful-api-main/src/route/api.js
+++ belajar-nodejs-restful-api-main/src/route/api.js
@@ -2,30 +2,37 @@ import express from "express";
 import userController from "../controller/user-controller.js";
 import contactController from "../controller/contact-controller.js";
 import addressController from "../controller/address-controller.js";
-import {authMiddleware} from "../middleware/auth-middleware.js";
+import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const userRouter = new express.Router();
 userRouter.use(authMiddleware);
 
 // User API
-userRouter.get('/api/users/current', userController.get);
-userRouter.patch('/api/users/current', userController.update);
-userRouter.delete('/api/users/logout', userController.logout);
+userRouter.get("/users/current", userController.get);
+userRouter.patch("/users/current", userController.update);
+userRouter.delete("/users/logout", userController.logout);
 
 // Contact API
-userRouter.post('/api/contacts', contactController.create);
-userRouter.get('/api/contacts/:contactId', contactController.get);
-userRouter.put('/api/contacts/:contactId', contactController.update);
-userRouter.delete('/api/contacts/:contactId', contactController.remove);
-userRouter.get('/api/contacts', contactController.search);
+userRouter.post("/contacts", contactController.create);
+userRouter.get("/contacts/:contactId", contactController.get);
+userRouter.put("/contacts/:contactId", contactController.update);
+userRouter.delete("/contacts/:contactId", contactController.remove);
+userRouter.get("/contacts", contactController.search);
 
 // Address API
-userRouter.post('/api/contacts/:contactId/addresses', addressController.create);
-userRouter.get('/api/contacts/:contactId/addresses/:addressId', addressController.get);
-userRouter.put('/api/contacts/:contactId/addresses/:addressId', addressController.update);
-userRouter.delete('/api/contacts/:contactId/addresses/:addressId', addressController.remove);
-userRouter.get('/api/contacts/:contactId/addresses', addressController.list);
+userRouter.post("/contacts/:contactId/addresses", addressController.create);
+userRouter.get(
+  "/contacts/:contactId/addresses/:addressId",
+  addressController.get
+);
+userRouter.put(
+  "/contacts/:contactId/addresses/:addressId",
+  addressController.update
+);
+userRouter.delete(
+  "/contacts/:contactId/addresses/:addressId",
+  addressController.remove
+);
+userRouter.get("/contacts/:contactId/addresses", addressController.list);
 
-export {
-    userRouter
-}
+export { userRouter };
