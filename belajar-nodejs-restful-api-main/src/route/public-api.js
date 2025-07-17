3c772b048d6f629122ec016cb366a4de7b924392
Juchase <jcsetiawan45@gmail.com>
2025-07-17 16:53:20 +0800

    fix user api path

--- belajar-nodejs-restful-api-main/src/route/public-api.js
+++ belajar-nodejs-restful-api-main/src/route/public-api.js
@@ -3,10 +3,8 @@ import userController from "../controller/user-controller.js";
 import healthController from "../controller/health-controller.js";
 
 const publicRouter = new express.Router();
-publicRouter.post('/api/users', userController.register);
-publicRouter.post('/api/users/login', userController.login);
-publicRouter.get('/ping', healthController.ping);
+publicRouter.post("/users", userController.register);
+publicRouter.post("/users/login", userController.login);
+publicRouter.get("/ping", healthController.ping);
 
-export {
-    publicRouter
-}
+export { publicRouter };
