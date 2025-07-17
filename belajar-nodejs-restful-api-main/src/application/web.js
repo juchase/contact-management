707cfc89b64374c993e76b5d001f00156a6193e3
Juchase <jcsetiawan45@gmail.com>
2025-07-17 19:04:59 +0800

    update url FE

--- belajar-nodejs-restful-api-main/src/application/web.js
+++ belajar-nodejs-restful-api-main/src/application/web.js
@@ -11,7 +11,7 @@ web.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://contact-management-iau9gmpg1-juchases-projects.vercel.app",
+      "https://contact-management-gt2l4eqwl-juchases-projects.vercel.app",
     ], // tambahkan frontend URL di sini
     credentials: true,
   })
