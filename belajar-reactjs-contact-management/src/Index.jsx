08c9ac1c9f5adbaf5e326781b0f226a84424d96f
Juchase <jcsetiawan45@gmail.com>
2025-07-20 00:58:55 +0800

    fix authorization

--- belajar-reactjs-contact-management/src/Index.jsx
+++ belajar-reactjs-contact-management/src/Index.jsx
@@ -2,7 +2,7 @@ import { useEffectOnce, useLocalStorage } from "react-use";
 import { useNavigate } from "react-router-dom";
 
 export default function Index() {
-  const [token, _] = useLocalStorage("token", "");
+  const [token, _] = useLocalStorage("token");
   const navigate = useNavigate();
 
   useEffectOnce(() => {
