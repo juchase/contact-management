b35701cb0e558a273b0d5bba1c35eb1093ddb543
Juchase <jcsetiawan45@gmail.com>
2025-07-20 00:55:42 +0800

    fix authorization

--- belajar-reactjs-contact-management/src/Index.jsx
+++ belajar-reactjs-contact-management/src/Index.jsx
@@ -1,31 +1,17 @@
 import { useEffectOnce, useLocalStorage } from "react-use";
-import { userDetail } from "./lib/api/UserApi";
 import { useNavigate } from "react-router-dom";
-import { alertError } from "./lib/alert";
 
 export default function Index() {
   const [token, _] = useLocalStorage("token", "");
   const navigate = useNavigate();
 
-  async function authorization() {
-    try {
-      const response = await userDetail(token);
-      const responseBody = await response.json();
-      if (response.status !== 200) {
-        throw new Error(responseBody);
-      }
-      return responseBody;
-    } catch {
-      await alertError("Anda belum login!");
-      navigate({
-        pathname: "/login",
-      });
-    }
-  }
-
   useEffectOnce(() => {
-    authorization();
-  });
+    if (token) {
+      navigate("/dashboard");
+    } else {
+      navigate("/login");
+    }
+  }, [token, navigate]);
 
-  return <></>;
+  return null;
 }
